6d46f10ff68364434c40adc0b71cecf6d292c584
Joshua Chin <joshuachin191@gmail.com>
2020-10-27 16:22:54 +0800

    Added ability to register a new user

--- api/routes.js
+++ api/routes.js
@@ -1,20 +1,45 @@
 import express from 'express';
+import expressValidator from 'express-validator';
+const {check, validationResult} = expressValidator;
 import {User} from './models.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+//GET /:user
+router.get('/:user', (req, res) => {
     console.log("hi");
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
+//POST /register
+//Create a new user
+router.post('/register', [
+    check('username')
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "username"'),
+    check('password')
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "password"'),
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map(error => error.msg);
+        return res.status(400).json({errors: errorMessages})
+    }
+
+    const {username, password} = req.body
+    const existingUser = await User.findOne({"username": username}).exec();
+
+    if (existingUser) {
+        console.log("user exists")
+        return res.status(400).json({errors: "Unsuccessful"});
+    }
+
     let user = new User (req.body);
     user.save((err, user) => {
         if (err) return next (err);
-        res.status(201);
-        res.json(user);
+        return res.status(201).end();
     })
 })
+
 
 export default router; 
