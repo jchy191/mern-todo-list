73ce6d6f654ebc8942ba9b32525ce9ba46c42572
Joshua Chin <joshuachin191@gmail.com>
2020-10-28 09:28:44 +0800

    Created some components, including Form component to be used as render prop

---
+++ client/src/Components/Form.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Form = (props) => {
+
+    const {
+        cancel,
+        errors,
+        submit,
+        submitButtonText,
+        elements,
+    } = props;
+    
+    const handleSubmit = (e) => {
+        submit();
+    }
+
+    const handleCancel = (e) => {
+        cancel();
+    }
+
+    return (
+
+        <div>
+            <ErrorsDisplay errors={errors}/>
+            <form onSubmit={handleSubmit}>
+                {elements()}
+                <div>
+                    <button type="submit">{submitButtonText}</button>
+                    <button onClick={handleCancel}>Cancel</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
+
+
+const ErrorsDisplay = ({errors}) => {
+    let errorsDisplay = null;
+    if (errors.length) {
+        errorsDisplay = (
+            <div>
+                <h2>Validation error:</h2>
+                <ul>
+                    {errors.map((error, i) => <li key={i}>{error}</li>)}
+                </ul>
+            </div>
+        )
+    }
+
+    return errorsDisplay
+}
---
+++ client/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+
+    return (
+        <h1>To-Do App</h1>
+    )
+
+}
+
+export default Header;
