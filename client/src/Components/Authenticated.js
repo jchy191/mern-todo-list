6162016054e5649a164d7db38baff3d7410a22b1
Joshua Chin <joshuachin191@gmail.com>
2020-11-05 16:18:10 +0800

    Added cookies

--- client/src/Components/Authenticated.js
+++ client/src/Components/Authenticated.js
@@ -1,15 +1,17 @@
 import React, {useContext} from 'react';
+import {Redirect} from 'react-router-dom';
 import {Context} from '../Context.js'
 
 
 const Authenticated = (props) => {
 
     const context = useContext(Context);
-    const user = context.authUser.name
+    const user = context.authUser
     return (
         <React.Fragment>
+            {user && <Redirect to={`/user/${user.username}`}/>}
             <h1> Authenticated </h1>
-            <p>Hi, {user}.</p>
+            <p>Hi, {user.name}.</p>
         </React.Fragment>
         
     )
--- client/src/Context.js
+++ client/src/Context.js
@@ -1,16 +1,18 @@
 import React, {useState} from 'react';
+import Cookies from 'js-cookie';
 import Data from './Data';
 
 const Context = React.createContext();
 
 const Provider = (props) => {
 
-    const [authUser, setAuthUser] = useState(null)
+    const [authUser, setAuthUser] = useState(Cookies.getJSON('authenticatedUser') || null);
 
     const signIn = async (credentials) => { 
-        const user = await Data.getUser(credentials)
+        const user = await Data.getUser(credentials);
         if (user !== null) {
-            setAuthUser(user)
+            setAuthUser(user);
+            Cookies.set('authenticatedUser', JSON.stringify(user), {expires: 1});
             return user;
         } 
         return null;
@@ -18,6 +20,7 @@ const Provider = (props) => {
 
     const signOut = () => {
         setAuthUser(null);
+        Cookies.remove('authenticatedUser');
     }
 
     const value = {
